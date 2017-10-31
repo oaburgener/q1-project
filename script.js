0f831a7ba85a79e9ebbdd6373c1578e1443d799b
Olivia <livburgener@gmail.com>
2017-10-31 13:42:46 -0600

    now suggests a jacket to wear based on what it feels like currently

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ $(document).ready(function(){
   });
 
   $(".button").on("click", function(){
-    //grabs city and state in search bar
+
+    //empties boxes filled with text and pictures
     $(".locationText").empty();
     $(".outfits").empty();
+    $(".extras").empty();
+
+    //grabs city and state in search bar
     let zipcode = $("#location-search")[0].value;
     if (zipcode === ""){
       alert("Please enter valid zip code. ex: 80027");
@@ -28,6 +32,9 @@ $(document).ready(function(){
       $(".locationText").append("It is currently: " + $degrees + " degrees fahrenheit, in " + $place);
       $(".locationText").append("Today's high will be: " + $high + " degrees fahrenheit");
 
+      let $feelsLike = data.current_observation.feelslike_f;
+      $(".extras").append("It feels like " + $feelsLike + " degrees outside right now so you might want to wear a...");
+
 
       let $xhr_1 = $.getJSON("http://api.shopstyle.com/api/v2/lists?pid=uid7364-40040942-41&userId=oaburgener");
 
@@ -38,27 +45,48 @@ $(document).ready(function(){
           for (var i = 0; i < 5; i++){
             $(".outfits").append('<img src="' +
             //winter outfits
-            data1.lists[1].favorites[i].product.image.sizes.Large.url + '">');
+            data1.lists[2].favorites[i].product.image.sizes.Large.url + '">');
           };
         }else if($high >= 40 && $high <= 60){
           for (var i = 0; i < 5; i++){
             $(".outfits").append('<img src="' +
-            //winter outfits
-            data1.lists[4].favorites[i].product.image.sizes.Large.url + '">');
+            //fall outfits
+            data1.lists[5].favorites[i].product.image.sizes.Large.url + '">');
           };
         }else if($high >= 60 && $high <= 80){
           for (var i = 0; i < 5; i++){
             $(".outfits").append('<img src="' +
-            //winter outfits
-            data1.lists[3].favorites[i].product.image.sizes.Large.url + '">');
+            //spring outfits
+            data1.lists[4].favorites[i].product.image.sizes.Large.url + '">');
           };
         }else if($high >= 80){
           for (var i = 0; i < 5; i++){
             $(".outfits").append('<img src="' +
-            //winter outfits
-            data1.lists[2].favorites[i].product.image.sizes.Large.url + '">');
+            //summer outfits
+            data1.lists[3].favorites[i].product.image.sizes.Large.url + '">');
           };
-        }
+        };
+
+        //feels like area so you can wear extra when it's cold in the morning
+        if($feelsLike <= 50){
+            $(".extras").append('<img src="' +
+            data1.lists[1].favorites[4].product.image.sizes.Large.url + '">');
+            $(".extras").append('<img src="' +
+            data1.lists[1].favorites[3].product.image.sizes.Large.url + '">');
+
+        }else if($feelsLike >= 50 && $feelsLike <= 65 ){
+            $(".extras").append('<img src="' +
+            data1.lists[1].favorites[2].product.image.sizes.Large.url + '">');
+            $(".extras").append('<img src="' +
+            data1.lists[1].favorites[3].product.image.sizes.Large.url + '">');
+            $(".extras").append('<img src="' +
+            data1.lists[1].favorites[0].product.image.sizes.Large.url + '">');
+        }else if($feelsLike >= 65){
+            $(".extras").append('<img src="' +
+            data1.lists[1].favorites[1].product.image.sizes.Large.url + '">');
+        };
+
+
       //end of second done function
       });
 
