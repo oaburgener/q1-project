56b6b6d46ce4b213eb6641b1b5224404eb3f14aa
Olivia <livburgener@gmail.com>
2017-11-01 11:07:08 -0600

    pics a random accessory

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
-let numberCode = JSON.parse(localStorage.getItem('zipcode'));
-if(localStorage.hasOwnProperty('zipcode')){
-  var message = prompt("Would you like to use your previous zipcode?");
-};
-if(message === "yes"){
-  $("#location-search")[0].value = numberCode;
-};
+//
+// let numberCode = JSON.parse(localStorage.getItem('zipcode'));
+// if(localStorage.hasOwnProperty('zipcode')){
+//   var message = prompt("Would you like to use your previous zipcode?");
+// };
+// if(message === "yes"){
+//   $("#location-search")[0].value = numberCode;
+// };
 
 
 $(document).ready(function(){
@@ -38,7 +38,8 @@ $(document).ready(function(){
       let $degrees = data.current_observation.temp_f;
       let $place = data.current_observation.display_location.full;
       let $high = parseInt(data.forecast.simpleforecast.forecastday[0].high.fahrenheit);
-      console.log(typeof $high);
+
+
       $(".locationText").append("It is currently: " + $degrees + " degrees fahrenheit, in " + $place);
       $(".locationText").append("Today's high will be: " + $high + " degrees fahrenheit");
 
@@ -57,46 +58,67 @@ $(document).ready(function(){
           for (var i = 0; i < 7; i++){
             $(".outfits").append('<img src="' +
             //winter outfits
-            data1.lists[2].favorites[i].product.image.sizes.Large.url + '">');
+            data1.lists[3].favorites[i].product.image.sizes.Large.url + '">');
           };
-        }else if($high >= 40 && $high <= 60){
+        }else if($high >= 40 && $high <= 65){
           for (var i = 0; i < 5; i++){
             $(".outfits").append('<img src="' +
             //fall outfits
-            data1.lists[5].favorites[i].product.image.sizes.Large.url + '">');
+            data1.lists[6].favorites[i].product.image.sizes.Large.url + '">');
           };
-        }else if($high >= 60 && $high <= 80){
+        }else if($high >= 65 && $high <= 80){
           for (var i = 0; i < 5; i++){
             $(".outfits").append('<img src="' +
             //spring outfits
-            data1.lists[4].favorites[i].product.image.sizes.Large.url + '">');
+            data1.lists[5].favorites[i].product.image.sizes.Large.url + '">');
           };
         }else if($high >= 80){
           for (var i = 0; i < 5; i++){
             $(".outfits").append('<img src="' +
             //summer outfits
-            data1.lists[3].favorites[i].product.image.sizes.Large.url + '">');
+            data1.lists[4].favorites[i].product.image.sizes.Large.url + '">');
           };
         };
 
         //feels like area so you can wear extra when it's cold in the morning
         if($feelsLike <= 50){
             $(".extras").append('<img src="' +
-            data1.lists[1].favorites[4].product.image.sizes.Large.url + '">');
+            data1.lists[2].favorites[4].product.image.sizes.Large.url + '">');
             $(".extras").append('<img src="' +
-            data1.lists[1].favorites[3].product.image.sizes.Large.url + '">');
+            data1.lists[2].favorites[3].product.image.sizes.Large.url + '">');
 
         }else if($feelsLike >= 50 && $feelsLike <= 65 ){
             $(".extras").append('<img src="' +
-            data1.lists[1].favorites[2].product.image.sizes.Large.url + '">');
+            data1.lists[2].favorites[2].product.image.sizes.Large.url + '">');
             $(".extras").append('<img src="' +
-            data1.lists[1].favorites[3].product.image.sizes.Large.url + '">');
+            data1.lists[2].favorites[3].product.image.sizes.Large.url + '">');
             $(".extras").append('<img src="' +
-            data1.lists[1].favorites[0].product.image.sizes.Large.url + '">');
+            data1.lists[2].favorites[0].product.image.sizes.Large.url + '">');
         }else if($feelsLike >= 65){
             $(".extras").append('<img src="' +
-            data1.lists[1].favorites[1].product.image.sizes.Large.url + '">');
+            data1.lists[2].favorites[1].product.image.sizes.Large.url + '">');
         };
+
+
+        //random accessory
+        function accessoryArray(){
+          let picturesArr = [];
+          for(var i = 0; i < 5; i++){
+            picturesArr.push(data1.lists[1].favorites[i].product.image.sizes.Large.url)
+          }
+          return picturesArr;
+        }
+        accessoryArray();
+
+
+        function randomAccessory(){
+          var pictures = accessoryArray();
+          return $(".accessories-pics").append('<img src="' + pictures[(Math.floor(Math.random() * pictures.length))] + '">');
+        }
+        randomAccessory();
+
+
+
 
 
       //end of second done function
